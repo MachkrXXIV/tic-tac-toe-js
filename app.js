98d470310113b01ce689350406832be01bd5a893
Tin <bachmach24k@gmail.com>
2022-07-29 15:10:34 -0500

    progress on logic

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const gameBoard = (() => {
 
   const clear = () => {
     for (let i = 0; i < _board.length; i++) {
-      board[i] = "";
+      board[i] = undefined;
     }
   };
 
   return {
-    getBoardSpot,
-    setBoardSpot,
+    getCell,
+    setCell,
     clear,
   };
 })();
@@ -27,12 +27,14 @@ const displayController = (() => {
   // affects what is on screen html
   const boardCells = document.querySelectorAll(".board-cell");
   const restartBtn = document.querySelector(".reset");
+  const p1Score = document.querySelector(".p1-score");
+  const p2Score = document.querySelector(".p2-score");
+  const turnIndicator = document.querySelector(".turn-header");
   boardCells.forEach((cell) => {
     cell.addEventListener("click", (e) => {
-      e.target.dataset.index;
+      gameController.e.target.dataset.index;
     });
   });
-  return;
 })();
 
 const playerFactory = (sign) => {
@@ -43,8 +45,43 @@ const playerFactory = (sign) => {
 };
 
 const gameController = () => {
-  const playerX = playerFactory("X");
-  const playerO = playerFactory("O");
-  let isPlaying = false;
+  const player1 = playerFactory("X");
+  const player2 = playerFactory("O");
+  let gameOver = false;
+  let roundNum = 1;
+  let currentTurnSign = "X";
+
+  const makeMove = (cellIndex) => {
+    gameBoard.setCell(cellIndex, currentTurnSign);
+    if (checkWin(cellIndex)) {
+      return;
+    }
+    if (round === 9) {
+      return;
+    }
+    round++;
+    return;
+  };
+
+  const checkWin = (cellIndex) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    // return true if win
+    return winConditions
+      .filter((combination) => combination.includes(cellIndex))
+      .some((possibleCombination) =>
+        possibleCombination.every((index) => gameBoard.getCell(index) === turn)
+      );
+  };
+  return { makeMove, checkWin };
 };
 // use contains for fa-x or fa-o
